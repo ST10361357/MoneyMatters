eb5cde5d931613aedaa297481c05494f3beaf45c
st10361357@rcconnect.edu.za <st10361357@rcconnect.edu.za>
2025-05-10 19:03:44 +0000

    single db instance the @ volatile ensures visibility in change to INSTANCE

---
+++ app/src/main/java/com/example/moneymatters/database/AppDb.kt
@@ -0,0 +1,40 @@
+package com.example.moneymatters.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.moneymatters.data.Category
+import com.example.moneymatters.data.Expense
+import com.example.moneymatters.data.Goal
+import com.example.moneymatters.data.User
+
+@Database(entities = [User::class, Category::class, Expense::class, Goal::class], version = 1)
+abstract class AppDb: RoomDatabase() {
+    abstract fun UserDao(): UserDao
+    abstract fun CategoryDao(): CategoryDao
+    abstract fun ExpenseDao(): ExpenseDao
+
+    abstract fun GoalDao(): GoalDao
+
+    //db instance
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDb? = null
+
+        fun getDb(context: Context): AppDb {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDb::class.java,
+                    "mm_db"
+                ).fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+}
